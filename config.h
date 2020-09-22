4fdd05f1271376e441e66076938004d0720bdd59
dk949 <david2000katz@gmail.com>
2020-09-22 15:50:54 +0100

    Added the oppacity flag to dmenu

--- config.h
+++ config.h
@@ -82,7 +82,7 @@ static const Layout layouts[] = {
 /* commands */
 static       char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-l", "20", "-c",
-                                  "-bw", "3",   "-x",   NULL };
+                                  "-bw", "3",   "-x", "-o", "0.8",   NULL };
 
 static const char *termcmd[] = { "alacritty",           NULL };
 static const char *lockcmd[] = { "slock",               NULL }; // Lock the screen with slock
