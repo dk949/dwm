5cc5a8cea12582dd11a43aaafc068dc57ba84c3c
dk949 <david2000katz@gmail.com>
2021-01-05 22:17:58 +0000

    Started on the CMake thing

--- config.mk
+++ config.mk
@@ -28,7 +28,6 @@ INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X12LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} ${XBACKLIGHTLIBS} -lX11-xcb -lxcb-res -lX11 # FIXME: What is going on here?
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
