ed7223da20967ca2699eeb9d5c6f84a6296bbc97
dk949 <david2000katz@gmail.com>
2021-01-05 22:25:13 +0000

    All still compiles, looking a little better

--- config.mk
+++ config.mk
@@ -8,7 +8,6 @@ PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -25,10 +24,9 @@ FREETYPEINC = /usr/include/freetype2
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${X12LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} ${XBACKLIGHTLIBS} -lX11-xcb -lxcb-res -lX11 # FIXME: What is going on here?
+LIBS = -lX11 ${XINERAMALIBS} ${FREETYPELIBS} ${XBACKLIGHTLIBS} -lX11-xcb -lxcb-res # FIXME: What is going on here?
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
