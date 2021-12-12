30a58c7f10e895328b1c115ef33819a356739efa
dk949 <56653556+dk949@users.noreply.github.com>
2021-12-13 00:56:18 +0200

    reformatted util

--- util.h
+++ util.h
@@ -1,8 +1,13 @@
 /* See LICENSE file for copyright and license details. */
+#ifndef UTIL_H
+#define UTIL_H
 
-#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#define MIN(A, B)               ((A) < (B) ? (A) : (B))
-#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+#define MAX(A, B)        ((A) > (B) ? (A) : (B))
+#define MIN(A, B)        ((A) < (B) ? (A) : (B))
+#define BETWEEN(X, A, B) ((A) <= (X) && (X) <= (B))
 
+#include <stddef.h>
 void die(const char *fmt, ...);
 void *ecalloc(size_t nmemb, size_t size);
+
+#endif  // UTIL_H
