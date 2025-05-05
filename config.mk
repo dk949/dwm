9a697450e21db95af794f2314168ebf166d03ec0
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-06 00:29:25 +0100

    updated makefile

--- config.mk
+++ config.mk
@@ -1,68 +1,35 @@
-# dwm version
+OUT_NAME = dwm
 
-ifndef VERSION
-	DATE         = $(shell git log -1 --format='%cd' --date=format:'%F')
-	DATE_TIME    = $(DATE) 00:00
-	COMMIT_COUNT = $(shell git rev-list --count HEAD --since="$(DATE_TIME)")
-	VERSION      = 6.2.$(shell date -d "$(DATE)" +'%Y%m%d')_$(COMMIT_COUNT)
-endif
+include version.mk
 
-# Customize below to fit your system
+CC         ?= gcc
+CXX        ?= g++
+AR         ?= ar
+PKG_CONFIG ?= pkg-config
 
-# paths
-DESTDIR   ?=
-PREFIX    ?= /usr/local
+SRC_DIR   = src
+BUILD_DIR = build
+
+# Turn (some) warnings into errors
+WARNINGS_AS_ERRORS=
+# Use address, leak and UB sanisizers
+USE_SANITIZERS=1
+# allow c and c++ extension
+USE_EXTENSIONS=
+
+# Generate a `compile_commands.json` file in the root of the project
+COMPILE_COMMANDS ?=
+
+# Possible values are DEBUG and RELEASE (case sensitive)
+MODE ?=
+# This value will be used if MODE is not specified
+DEFAULT_MODE = DEBUG
+
+include libs.mk
+
+# Install directories. Defaults to out/ in the root of the project
+# To install system wide by default, set DESTDIR= PREFIX=/usr
+DESTDIR ?= ./
+PREFIX  ?= out
 MANPREFIX  = $(PREFIX)/share/man
-MODE      ?= RELEASE
 
-# Optional dependencies:
-# 	libxinerama
-#   libasound
-#   st
-# Will be included if installed
-XINERAMAFLAGS = $(shell pkg-config xinerama --cflags --silence-errors && echo "-DXINERAMA")
-XINERAMALIBS  = $(shell pkg-config xinerama --libs --silence-errors)
-
-ASOUNDFLAGS = $(shell pkg-config alsa --cflags --silence-errors && echo -DASOUND)
-ASOUNDLIBS  = $(shell pkg-config alsa --libs --silence-errors)
-
-STFLAGS = $(shell command -v st >/dev/null && echo -DST_INTEGRATION)
-
-# Optional features:
-# 	Setting backlight with X
-# Deprecated. Uncomment to reenable
-# XBACKLIGHTLIBS = `pkg-config xcb xcb-randr xcb-util --cflags --silence-errors && echo "-DXBACKLIGHT"`
-# XBACKLIGHTFLAGS = `pkg-config xcb xcb-randr xcb-util --libs --silence-errors`
-
-
-REQ_LIBS = x11 x11-xcb xcb-res xft fontconfig
-
-# includes and libs
-LIBFLAGS = $(XINERAMAFLAGS) $(ASOUNDFLAGS) $(XBACKLIGHTFLAGS) `pkg-config $(REQ_LIBS) --cflags`
-LIBS     = $(XINERAMALIBS)  $(ASOUNDLIBS)  $(XBACKLIGHTLIBS)  `pkg-config $(REQ_LIBS) --libs`
-
-RELEASE_CPPFLAGS = -DNDEBUG
-DEBUG_CPPFLAGS   =
-TRACE_CPPFLAGS   = $(DEBUG_CPPFLAGS) -DDWM_TRACE_EVENTS
-
-# flags
-CPPFLAGS = -D_DEFAULT_SOURCE         \
-		   -D_GNU_SOURCE             \
-		   -D_POSIX_C_SOURCE=200809L \
-		   -DVERSION=\"$(VERSION)\"  \
-		   $(STFLAGS)	             \
-		   $($(MODE)_CPPFLAGS)
-
-RELEASE_CFLAGS = -Os -flto
-DEBUG_CFLAGS   = -Og -g -fsanitize=address
-TRACE_CFLAGS   = $(DEBUG_CFLAGS)
-RELEASE_LDFLAGS = -flto
-DEBUG_LDFLAGS   = -lg -fsanitize=address
-TRACE_LDFLAGS   = $(DEBUG_LDFLAGS)
-
-CFLAGS         = -std=c23 -Wpedantic -Wall -Werror $($(MODE)_CFLAGS) $(LIBFLAGS) $(CPPFLAGS)
-
-LDFLAGS  = $($(MODE)_LDFLAGS) $(LIBS)
-
-# compiler and linker
-CC ?= gcc
---
+++ libs.mk
@@ -0,0 +1,11 @@
+XINERAMAFLAGS = $(shell pkg-config xinerama --cflags --silence-errors && echo "-DXINERAMA")
+XINERAMALIBS  = $(shell pkg-config xinerama --libs --silence-errors)
+
+ASOUNDFLAGS = $(shell pkg-config alsa --cflags --silence-errors && echo -DASOUND)
+ASOUNDLIBS  = $(shell pkg-config alsa --libs --silence-errors)
+
+PKG_CONFIG_LIBS = x11 x11-xcb xcb-res xft fontconfig
+# Include directories (-I/dir/name or -isystem/dir/name)
+INC=$(XINERAMAFLAGS) $(ASOUNDFLAGS)
+# Libraries to be linked (-llibname or -L/dir/name)
+LIBS=$(XINERAMALIBS) $(ASOUNDLIBS)
