c7bc4582dc464d81c835ff210c89d1e0a681a896
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-20 22:12:53 +0100

    updated base version

--- version.mk
+++ version.mk
@@ -2,5 +2,5 @@ ifndef VERSION
 DATE         = $(shell git log -1 --format='%cd' --date=format:'%F')
 DATE_TIME    = $(DATE) 00:00
 COMMIT_COUNT = $(shell git rev-list --count HEAD --since="$(DATE_TIME)")
-VERSION      = 6.2.$(shell date -d "$(DATE)" +'%Y%m%d')_$(COMMIT_COUNT)
+VERSION      = 6.3.$(shell date -d "$(DATE)" +'%Y%m%d')_$(COMMIT_COUNT)
 endif
