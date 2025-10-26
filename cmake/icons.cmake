19b120125340683914a59dd1b3a00a52311ef084
dk949 <56653556+dk949@users.noreply.github.com>
2025-10-26 13:51:18 +0000

    make sure icons also get installed

--- cmake/icons.cmake
+++ cmake/icons.cmake
@@ -1,5 +1,17 @@
-set(ICONPREFIX "${CMAKE_INSTALL_PREFIX}/share/pixmaps" CACHE STRING "Compiler cache to be used")
-set(ICONDIR "${ICONPREFIX}" CACHE STRING "Compiler cache to be used")
-function(target_add_icon_loc target access)
-    target_compile_definitions(${target} ${access} -DICONDIR="${ICONDIR}")
-endfunction()
+set(ICONPREFIX
+    "${CMAKE_INSTALL_PREFIX}/share/pixmaps"
+    CACHE INTERNAL ""
+)
+function (target_add_icon_loc target access)
+    target_compile_definitions(${target} ${access} -DICONDIR="${ICONPREFIX}")
+endfunction ()
+function (install_icons)
+    install(
+        FILES
+              ${CMAKE_SOURCE_DIR}/icons/dwm-error.svg
+              ${CMAKE_SOURCE_DIR}/icons/dwm-icon-black.svg
+              ${CMAKE_SOURCE_DIR}/icons/dwm-icon.svg
+              ${CMAKE_SOURCE_DIR}/icons/dwm-warning.svg
+        DESTINATION ${ICONPREFIX}
+    )
+endfunction ()
